aeeb39bd47fe34493ec8523cc3ebbf726e297473
andywukoa <andywu@koafaith.com>
2025-08-13 11:23:40 +0800

    refactor: 將三個 interface 中重複的 `thumbnail` 屬性，提取成一個 `HasThumbnail` interface。

--- interfaces/past.interface.ts
+++ interfaces/past.interface.ts
@@ -1,14 +1,13 @@
+ts
 /**
  * 獲獎團隊
  */
-export interface PastWinningTeam {
+export interface PastWinningTeam extends HasThumbnail {
   id: number;
   /** 名次 */
   ranking: string;
   /** 團隊名稱 */
   team_name: string;
-  /** 回顧縮圖 */
-  thumbnail: string;
   /** 團隊成員 */
   team_members: string;
   /** 團隊簡介 */
@@ -23,12 +22,10 @@ export interface PastWinningTeam {
 /**
  * 照片回顧
  */
-export interface PastPhoto {
+export interface PastPhoto extends HasThumbnail {
   id: number;
   /** 回顧名稱 */
   title: string;
-  /** 回顧縮圖 */
-  thumbnail: string;
   /** 多圖連結 */
   image_list: string[];
   /** 更多照片連結 */
@@ -38,14 +35,20 @@ export interface PastPhoto {
 /**
  * 參賽影音回顧
  */
-export interface PastVideo {
+export interface PastVideo extends HasThumbnail {
   id: number;
   /** 日期 */
   date: string;
   /** 回顧名稱 */
   title: string;
-  /** 回顧縮圖 */
-  thumbnail: string;
   /** 回顧影片連結 */
   video_url: string;
+}
+
+/**
+ * 擁有縮圖
+ */
+interface HasThumbnail {
+  /** 回顧縮圖 */
+  thumbnail: string;
 }
